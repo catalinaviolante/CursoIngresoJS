9403a88e08aac6bceed485f933833927902282e7
catalina.violante <catalina.violante@gmail.com>
2020-07-13 22:21:30 -0300

    Clase 1, EJ: 1 a 6 de E/S

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,11 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	var dato;
+
+	dato = prompt ("ingrese nombre");
+
+   	alert("su nombre es " + dato );
+
 }
 
